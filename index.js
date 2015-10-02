604b06b3fc4afb8f8d36381d3ac872ef7691b033
Christian Gati <christian.gati@gmail.com>
2015-10-02 00:32:20 -0400

    Add concurrent smooth drawing

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ var rooms = {};
 var room_drawing = {};
 
 app.use(express.static('static'));
+
+app.get('/admin', function(req, res) {
+    response = "";
+    for (var key in room_drawing) {
+        var room = rooms[key];
+        response += key + '\n';
+    }
+    res.end(response);
+});
+
 app.get('*', function(req, res) {
     res.sendFile(__dirname + '/index.html');
 });
@@ -15,21 +25,27 @@ io.on('connection', function(client) {
     client.on('join', function(pathname) {
         rooms[client.id] = pathname;
         client.join(rooms[client.id]);
-
+        io.to(client.id).emit('welcome', client.id);
         draw_history = room_drawing[rooms[client.id]];
         if (draw_history !== undefined) {
             io.to(rooms[client.id]).emit('persist', draw_history);
         }
     });
 
-    client.on('movement', function(coords) {
+    client.on('path', function(list) {
         draw_history = room_drawing[rooms[client.id]];
         if (draw_history === undefined) {
-            draw_history = [];
+            draw_history = Array();
         }
-        draw_history.push(coords);
+        draw_history.push.apply(draw_history, list);
         room_drawing[rooms[client.id]] = draw_history;
-        io.to(rooms[client.id]).emit('movement', coords);
+        io.to(rooms[client.id]).emit(
+            'path', list
+        );
+    });
+
+    client.on('draw', function(path) {
+        io.to(rooms[client.id]).emit('draw', path);
     });
 
     client.on('clear', function() {
